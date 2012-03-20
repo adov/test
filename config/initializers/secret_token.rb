52864723cff8cc9e3737dbf1ec77fe39d0864b36
AD <666@adovad.ru>
2012-03-20 20:13:35 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'da22473231ddbabef0d43f8739a587a93db13bbac2e7d32789884cd312e013b52ff5920e1f23e4637fc72a96f40fdbee54ca26c76617370b7bd52a0da53a843a'
